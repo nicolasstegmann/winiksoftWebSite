72a87cca8795f5dd46fd8847e18ec300c6c045f4
nicolas.stegmann <nicolas.stegmann@winiksoft.com>
2022-01-27 11:09:01 -0300

    win-27: add form send simulation

---
+++ scripts/contacto.php
@@ -0,0 +1,12 @@
+<?php
+$destino= "info@winiksoft.com";
+/*
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$comentarios = $_POST["comentarios"];
+$contenido = "Nombre: "  . $nombre . "\nApellido: "  . $apellido ."\nCorreo: " .$correo. "\nTelefono: " . $telefono . "\nComentarios: " .$comentarios;
+mail($destino,"contacto",$contenido);
+*/
+?>
---
+++ scripts/recruiting.php
@@ -0,0 +1,14 @@
+<?php
+$destino= "recruiting@winiksoft.com";
+/*
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$comentarios = $_POST["comentarios"];
+
+$contenido = "Nombre: "  . $nombre . "\nApellido: "  . $apellido ."\nCorreo: " .$correo. "\nTelefono: " . $telefono . "\nComentarios: " .$comentarios;
+
+mail($destino,"contacto",$contenido);
+*/
+?>
